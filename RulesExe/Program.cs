e5c4eaa9b2150f2677306571a4b350c5dc35c142
Citizen <codeaiplings@gmail.com>
2024-02-24 20:24:30 +0200

    base commit

---
+++ RulesExe/Program.cs
@@ -0,0 +1,17 @@
+﻿using AI.Domain.RulesEngine;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var lambdaList = new List<string>();
+        var lambda = "t.TransactionAmount >= u.TransactionThereshold";
+        lambdaList.Add(lambda);
+        _ = RulesEngine.ProcessLambdas(lambdaList);
+    }
+}
