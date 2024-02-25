ecacf72c4568a0d5a8097e8aa2a3088a3abcef68
Citizen <codeaiplings@gmail.com>
2024-02-25 21:23:29 +0200

    Added Node Aggregation

---
+++ AI.Engine/RuleEfficacyNodeBasic.cs
@@ -0,0 +1,54 @@
+﻿using AI.Engine.Interfaces;
+
+using AI.Infrastructure.Data;
+
+
+namespace AI.Engine
+
+{
+
+    //Fork to decide the tree paths in original Rules Engine
+
+
+
+
+
+    public class RuleEfficacyNodeBasic
+
+    {
+
+
+
+        private readonly IDecider _decider;
+
+
+
+        public bool _isFinalNode { get; private set; } = false;
+
+        private IAggregator _aggregate;
+
+        private Action _action;
+
+        public RuleEfficacyNodeBasic(IList<double> rowValues, IAggregator aggregate, IDecider decider, Action action,
+
+             bool IsFinalNode)
+
+        {
+
+            _aggregate = aggregate;
+
+            _decider = decider;
+
+            _isFinalNode = IsFinalNode;
+
+            Dro.RowValues = rowValues;
+
+            _action = action;
+
+        }
+
+
+
+
+    }
+}
